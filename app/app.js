9a145c846b2e5bb476afbae1dcbce51ceaec1b00
apivetta <aguspivetta@gmail.com>
2016-07-06 14:04:11 +0000

    scaffold

---
+++ app/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('dashboard-semaforos', [])
+  .config(function() {
+
+  });
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+//var templateCache = require('gulp-angular-templatecache');
+
+var paths = {
+  sass: ['./app/*.scss', './app/components/**/*.scss', './www/shared/**/*.scss'],
+  //templates: ['./www/*.html', './www/modules/**/*.html', './www/core/**/*.html']
+};
+
+gulp.task('default', ['sass', /*'template-cache',*/ 'watch']);
+
+/*gulp.task('template-cache', function() {
+  return gulp.src(paths.templates)
+    .pipe(templateCache({ standalone: true }))
+    .pipe(gulp.dest('./www/core'));
+});*/
+
+gulp.task('sass', function(done) {
+  gulp.src('./app/styles.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./app/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./app/'))
+    .on('end', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+  //gulp.watch(paths.templates, ['template-cache']);
+});
