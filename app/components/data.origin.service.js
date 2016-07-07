8fea5a0ffdcae6e83f947cc5180a57081e7efe51
javier <javrod83@gmail.com>
2016-07-07 13:04:23 -0300

    scafold para todos

--- app/components/data.origin.service.js
+++ app/components/data.origin.service.js
@@ -1,6 +1,32 @@
 'use strict';
-angular.module('dashboard-semaforos').factory('DataOriginService', ['', DataOriginService]);
+angular.module('dashboard-semaforos').factory('DataOriginService', ['$http', DataOriginService]);
 
-function DataOriginService() {
-  return {}
+function DataOriginService($http) {
+
+
+	var tabla = undefined ;
+
+	// $http({
+	//   method: 'GET',
+	//   url: '/assets/data/datos.json'
+	// }).then(function successCallback(response) {
+	//     // this callback will be called asynchronously
+	//     // when the response is available
+	//     console.log(response);
+	//   }, function errorCallback(response) {
+	//     // called asynchronously if an error occurs
+	//     // or server returns response with an error status.
+	//   	console.log(response);
+	//   });
+
+	function getTable(){
+
+		return $http({
+		  method: 'GET',
+		  url: '/assets/data/datos.json'
+		});
+
+	}
+
+  return {getTable:getTable};
 }
