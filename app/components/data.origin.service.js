1c609a7fb6d64d577d1dd23d4a37b117b1759f27
javier <javrod83@gmail.com>
2016-07-11 14:40:27 -0300

    los tuyos los mios , los nuestros

--- app/components/data.origin.service.js
+++ app/components/data.origin.service.js
@@ -25,42 +25,30 @@ RESULTADO
 	function getTable(){
 		var promise = $q(function (resolve,reject){
 
-		function success(response){
-			
-			var data =response.data;
-			$(data).each(function(i,e){
+			function success(response){
+				var data =response.data;
+				$(data).each(function(i,e){
+					console.log("cantidad: "+ e['CANTIDAD'] +" p. unit: " +e['P. UNIT']);
+					var temp =   e['CANTIDAD'] * e['P. UNIT'];
+					console.log("PRESUP 2010: ["+temp+"]")
+				});
+				resolve(data)
+			}
 
+			function fail(response){
+				console.log(response);
+				reject(response);
+			}
 
-
-				console.log("cantidad: "+ e['CANTIDAD'] +" p. unit: " +e['P. UNIT']);
-
-				var temp =   e['CANTIDAD'] * e['P. UNIT'];
-
-				console.log("PRESUP 2010: ["+temp+"]")
-
-			});
-
-			resolve(data)
-		}
-
-		function fail(response){
-			console.log(response);
-			reject(response);
-		}
-
-		$http({
-		  method: 'GET',
-		  url: '/assets/data/datos.json'
-		}).then(success,fail);
+			$http({
+			  method: 'GET',
+			  url: '/assets/data/datos.json'
+			}).then(success,fail);
 
 
 		})
 
-
-
 		return promise;
-
 	}
-
   return {getTable:getTable};
 }
