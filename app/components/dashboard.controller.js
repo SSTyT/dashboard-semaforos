92b74ffd632058076fa9b21a0e611e163662ac6c
javier <javrod83@gmail.com>
2016-07-12 02:01:14 -0300

    updatear updatea , falta un buffer de edicion en los coeficientes

--- app/components/dashboard.controller.js
+++ app/components/dashboard.controller.js
@@ -10,14 +10,14 @@ function DashboardController($scope,$q,DataOriginService) {
 EMPRESA
 RUBRO
 SUBRUBRO
-DESCRIPCIÓN
+description
 SUBITEM
-U.M.
+UnidadMedida
 CANTIDAD
-P. UNIT
-PRESUP. 2010
+precioUnitario
+p2010
 AJUSTE
-NUEVO PRECIO ESTIMADO
+NPE
 RESULTADO
 */
   $scope.grid = {
@@ -28,46 +28,28 @@ RESULTADO
           { name:'Descripcion', field: 'description',enableCellEdit: false },
           { name:'SubItem', field: 'SUBITEM' ,enableCellEdit: false},
           { name:'U. M.', field: 'UnidadMedida',enableCellEdit: false },
-          { name:'Cantidad', field: 'CANTIDAD' ,enableCellEdit: false},
-          { name:'Precio U', field: 'precioUnitario' ,enableCellEdit: false},
+          { name:'Cantidad', field: 'CANTIDAD' ,enableCellEdit: true},
+          { name:'Precio U', field: 'precioUnitario' ,enableCellEdit: true},
           { name:'Precio 2010', field: 'p2010' ,enableCellEdit: false},
           { name:'Ajuste', field: 'AJUSTE'},
           { name:'Estimado', field: 'NPE'},
           { name:'Resultado', field: 'RESULTADO' ,enableCellEdit: false}
-
-
-
                   ],
         data:[]
-
   };
-  $scope.saveRow =function (entity){
-    console.log(entity);
 
 
-    var promise = $q(function(success,fail){
-
-      success("GATO");
-      entity.RESULTADO="MIRA COMO CAMBIO TODO";
-    });
-  }
   $scope.grid.onRegisterApi = function(gridApi){
     //set gridApi on scope
     $scope.gridApi = gridApi;
     gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
   };
  
-
-
   DataOriginService.getTable().then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
     console.log(response);
     $scope.grid.data = response;
 
   }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
     console.log(response);
   });
 
@@ -79,18 +61,82 @@ RESULTADO
     console.log(response);
   });
 
+  $scope.saveRow =function (row){
+    console.log(row);
+    row.p2010=row.precioUnitario*row.CANTIDAD;
+    var promise = $q.defer();
 
+    $scope.gridApi.rowEdit.setSavePromise( row, promise.promise );
 
-  $scope.rubroCambio = function (rubro){
-    console.log("gato");
-    console.log(rubro);
+    promise.resolve();
   }
-$scope.subrubroCambio =  function (subrubro){
-  console.log("perro");
-  console.log(subrubro);
-}
+
+  $scope.grid.onRegisterApi = function(gridApi){
+    //set gridApi on scope
+    $scope.gridApi = gridApi;
+    gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+  };
+
+  $scope.updateRowsMatching = function updateRowsMatching(field,value){
+    var rows  = $scope.grid.data;
+    console.log(field + ""+ value);
+    for (var i = rows.length - 1; i >= 0; i--){
+        if (value.id == "GENERAL" || rows[i][field] == value.id ){
+          console.log("match row: "+i);
+          evalAndUpdate(rows[i]);
+        }
+    }
+  }
 
 
-
+  function evalAndUpdate(row)
+  {
+    if (row.NPE !== "")
+      { row.RESULTADO = row.NPE; }
+    else if (row.AJUSTE !== "")
+      { row.RESULTADO = row.AJUSTE * row.p2010; }
+    else if ($scope.coeficientes["GENERAL"].value !== "")
+      { row.RESULTADO = row.p2010 * $scope.coeficientes["GENERAL"].value; }
+    else if (($scope.coeficientes[row.RUBRO].value !== ""))
+      { row.RESULTADO = row.p2010 * $scope.coeficientes[row.RUBRO].value; }
+    else if (($scope.coeficientes[row.RUBRO].subrubro[row.SUBRUBRO].value !== ""))
+      { row.RESULTADO = row.p2010 * $scope.coeficientes[row.RUBRO].subrubro[row.SUBRUBRO].value; }
+    else
+      { row.RESULTADO = ""; }
+  }
   console.log("READY!");
 }
+
+
+/*
+
+condiciones  orden 
+
+    npe      =  { resultado =  npe }
+
+    ajuste   =  { resultado =  p2010 * AJUSTE}
+    
+    general  =  { resultado =  p2010 * COE_GENERAL}
+  
+    rubro    =  { resultado =  p2010 * RUBRO}
+    
+    subrubro =  { resultado =  p2010 * SUBRUBRO}
+
+    igual   
+    
+*/
+
+  /*
+EMPRESA
+RUBRO
+SUBRUBRO
+description
+SUBITEM
+UnidadMedida
+CANTIDAD
+precioUnitario
+p2010
+AJUSTE
+NPE
+RESULTADO
+*/
