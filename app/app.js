8a45ac39fa4aaffcd808e8a77b921e652a830855
javier <javrod83@gmail.com>
2016-07-12 13:54:29 -0300

    los coeficientes toman los cambios al presionar enter

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('dashboard-semaforos', ['templates', 'ui.grid','ui.grid.edit','ui.grid.rowEdit', 'ui.grid.cellNav'])
+var app = angular.module('dashboard-semaforos', ['templates', 'ui.grid','ui.grid.edit','ui.grid.rowEdit', 'ui.grid.cellNav'])
   .config(function() {
 
   });
